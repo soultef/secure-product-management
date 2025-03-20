a3ad529e90505b417a36a1850498d1fa37308929
soultef <soul_tefera@yahoo.com>
2025-03-20 11:17:18 -0500

    ecommerce product managment project is created

---
+++ src/main/java/com/soultech/productmanagement/SecureProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.soultech.productmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureProductManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureProductManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soultech/productmanagement/SecureProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soultech.productmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureProductManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
