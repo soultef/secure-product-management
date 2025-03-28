de93df1b94475020419057cc323ab7171ccf44b7
soultef <soul_tefera@yahoo.com>
2025-03-28 09:43:37 -0500

    Implemented User_Profile entity with attributes for user identification and role management

---
+++ src/main/java/com/soultech/productmanagement/model/User_Profile.java
@@ -0,0 +1,54 @@
+package com.soultech.productmanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User_Profile {
+    @Id
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
