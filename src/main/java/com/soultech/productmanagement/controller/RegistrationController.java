1744ce8c203a5783e7574f700d1923b6c884bfa4
soultef <soul_tefera@yahoo.com>
2025-04-01 14:22:33 -0500

    updated>>>>

---
+++ src/main/java/com/soultech/productmanagement/controller/RegistrationController.java
@@ -0,0 +1,13 @@
+package com.soultech.productmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/registration")
+    public String getRegistrationPage() {
+        return "registration";
+    }
+}
