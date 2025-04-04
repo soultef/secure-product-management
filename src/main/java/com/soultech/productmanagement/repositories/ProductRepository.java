996a6925370d48bfdac1cc231647584011ffad93
soultef <soul_tefera@yahoo.com>
2025-04-04 10:15:18 -0500

    feat(repository): add search query method to find products by name.

--- src/main/java/com/soultech/productmanagement/repositories/ProductRepository.java
+++ src/main/java/com/soultech/productmanagement/repositories/ProductRepository.java
@@ -3,6 +3,9 @@ package com.soultech.productmanagement.repositories;
 import com.soultech.productmanagement.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * Repository interface for interacting with the Product entity in the database.
  * * @author Solomon Demisse
@@ -11,4 +14,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // find product by keyword and case-insensitive
+    List<Product> findByNameContainingIgnoreCase(String name);
 }
