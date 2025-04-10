c4f3a4d7cc0a90278deba275badc3246d29fed88
soultef <soul_tefera@yahoo.com>
2025-04-10 12:34:58 -0500

    feat(controller): add post controller for login form and redirect to login avoids resubmission of the form if the user refreshes the page.

--- src/main/java/com/soultech/productmanagement/controller/LoginController.java
+++ src/main/java/com/soultech/productmanagement/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.soultech.productmanagement.model.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +15,15 @@ public class LoginController {
     @GetMapping("/login")
     public String getLoginPage(Model model) {
         model.addAttribute("user", new Users());
-        return "Home";
+        return "home";
+    }
+
+    @PostMapping("/form-submit")
+    public String loginForm(@ModelAttribute Users user)
+    {
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        return "redirect:/api/login";
+
     }
 }
