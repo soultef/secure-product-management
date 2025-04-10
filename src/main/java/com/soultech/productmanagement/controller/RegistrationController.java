be5b48d147a0d4f93c7180e827faec9ce7396b01
soultef <soul_tefera@yahoo.com>
2025-04-10 10:54:48 -0500

    feat(controller): set base path to /api using @RequestMapping.

--- src/main/java/com/soultech/productmanagement/controller/RegistrationController.java
+++ src/main/java/com/soultech/productmanagement/controller/RegistrationController.java
@@ -2,12 +2,16 @@ package com.soultech.productmanagement.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class RegistrationController {
 
     @GetMapping("/registration")
     public String getRegistrationPage() {
         return "registration";
     }
+
 }
